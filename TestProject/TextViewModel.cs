3550894cbbe75565f344a48b5cac3a66b14d4a8e
James LaPenn <jlapenn@loomissayles.com>
2024-08-27 14:54:14 -0400

    Add test project for development

---
+++ TestProject/TextViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MVVMGenerator.Attributes;
+
+namespace TestProject
+{
+    public partial class TestViewModel
+    {
+        [AutoCommand(nameof(CanTest))]
+        public void Test(int input)
+        {
+        }
+
+        public bool CanTest(int input) => true;
+    }
+}
