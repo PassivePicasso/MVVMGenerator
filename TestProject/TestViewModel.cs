31f8d4354901d3557d56769cd8aa33351c76bb50
James LaPenn <jlapenn@loomissayles.com>
2024-08-27 15:31:07 -0400

    Update TestViewModel tests/name

---
+++ TestProject/TestViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MVVMGenerator.Attributes;
+
+namespace TestProject
+{
+    public partial class TestViewModel
+    {
+        [AutoNotify] public bool isOpen;
+
+        [AutoCommand(nameof(CanExecuteClose))]
+        public void Close() => IsOpen = false;
+        public bool CanExecuteClose() => IsOpen;
+
+
+        [AutoCommand(nameof(CanTest))]
+        public void Test(int input)
+        {
+        }
+
+        public bool CanTest(int input) => true;
+
+        [AutoCommand]
+        public void ExecuteStuff()
+        {
+
+        }
+
+        [AutoCommand]
+        public static void TestStatic()
+        {
+
+        }
+    }
+}
