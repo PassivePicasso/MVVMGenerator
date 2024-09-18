7b5f3bf9d64b5cfc981b02af81c2ecd17fe5f950
James LaPenn <jlapenn@loomissayles.com>
2024-09-18 12:16:17 -0400

    Begin adding analyzers and fixes to improve UX

---
+++ MVVMGenerator/Analyzers/AutoCommandAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MVVM.Generator.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AutoCommandAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AutoCommandAnalyzer";
+        private static readonly LocalizableString Title = "AutoCommandAttribute usage";
+        private static readonly LocalizableString MessageFormat = "The method '{0}' is not public but has the AutoCommandAttribute";
+        private static readonly LocalizableString Description = "Methods with AutoCommandAttribute must be public.";
+        private const string Category = "Usage";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.MethodDeclaration);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+
+            var attributes = methodDeclaration.AttributeLists.SelectMany(al => al.Attributes);
+            foreach (var attribute in attributes)
+            {
+                var symbolInfo = context.SemanticModel.GetSymbolInfo(attribute);
+                if (symbolInfo.Symbol?.ContainingType.Name == "AutoCommandAttribute")
+                {
+                    var methodSymbol = context.SemanticModel.GetDeclaredSymbol(methodDeclaration);
+                    if (methodSymbol.DeclaredAccessibility != Accessibility.Public)
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, methodDeclaration.Identifier.GetLocation(), methodSymbol.Name);
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ MVVMGenerator/CodeFixers/AutoCommandCodeFixProvider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using MVVM.Generator.Analyzers;
+
+using Document = Microsoft.CodeAnalysis.Document;
+
+namespace MVVM.Generator.CodeFixers;
+
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AutoCommandCodeFixProvider)), Shared]
+public class AutoCommandCodeFixProvider : CodeFixProvider
+{
+    private const string title = "Make method public";
+
+    public sealed override ImmutableArray<string> FixableDiagnosticIds
+    {
+        get { return ImmutableArray.Create(AutoCommandAnalyzer.DiagnosticId); }
+    }
+
+    public sealed override FixAllProvider GetFixAllProvider()
+    {
+        return WellKnownFixAllProviders.BatchFixer;
+    }
+
+    public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var diagnostic = context.Diagnostics.First();
+        var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+        var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
+
+        context.RegisterCodeFix(
+            CodeAction.Create(
+                title: title,
+                createChangedSolution: c => MakePublicAsync(context.Document, declaration, c),
+                equivalenceKey: title),
+            diagnostic);
+    }
+
+    private async Task<Solution> MakePublicAsync(Document document, MethodDeclarationSyntax methodDecl, CancellationToken cancellationToken)
+    {
+        var modifiers = methodDecl.Modifiers;
+        var publicModifier = SyntaxFactory.Token(SyntaxKind.PublicKeyword);
+        var newModifiers = modifiers.Add(publicModifier);
+
+        var newMethodDecl = methodDecl.WithModifiers(newModifiers);
+
+        var root = await document.GetSyntaxRootAsync(cancellationToken);
+        var newRoot = root.ReplaceNode(methodDecl, newMethodDecl);
+
+        return document.Project.Solution.WithDocumentSyntaxRoot(document.Id, newRoot);
+    }
+}
