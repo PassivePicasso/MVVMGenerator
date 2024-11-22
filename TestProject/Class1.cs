307b79d86de22a55a297bcae85aaeec568fdd66c
James LaPenn <jlapenn@loomissayles.com>
2024-11-22 15:27:20 -0500

    update Class1 test to Person test

--- TestProject/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using MVVM.Generator.Attributes;
-
-namespace TestViewModels
-{
-    public partial class Class1
-    {
-        [AutoNotify] private string name;
-    }
-}
---
+++ TestProject/Person.cs
@@ -0,0 +1,13 @@
+﻿using MVVM.Generator.Attributes;
+
+namespace TestViewModels
+{
+    public partial class Person
+    {
+        [AutoNotify] private string firstName;
+        [AutoNotify] private string lastName;
+
+        [DependsOn(nameof(firstName), nameof(lastName))]
+        public string FullName => $"{firstName} {lastName}";
+    }
+}
